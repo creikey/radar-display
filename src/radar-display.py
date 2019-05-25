93d3ba8f8990f523e5b87cd81c060eedaed37fc4
Cameron Reikes <cameronreikes@gmail.com>
2019-05-25 07:28:37 -0700

    Add basic working image under label

--- src/radar-display.py
+++ src/radar-display.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 
-from PySide2.QtCore import Qt, Signal, QObject, Slot
 from PySide2 import QtCore, QtWidgets, QtGui
+from PySide2.QtCore import Qt, Signal, QObject, Slot
+from PySide2.QtGui import QImage, QPixmap
 import sys
 import logging
 import coloredlogs
 
 STYLESHEET_NAME = "stylesheet.qss"
+IMAGE_DIMENSIONS = [16, 16]
+
+
+class RadarImage(QtWidgets.QLabel):
+    def __init__(self, *args):
+        super().__init__(*args)
+        image = QImage(3, 3, QImage.Format_Grayscale8)
+        self.setPixmap(QPixmap.fromImage(image))
+        self.show()
+
 
 def main():
-    coloredlogs.install(level='DEBUG')
+    coloredlogs.install(level="DEBUG")
 
     app = QtWidgets.QApplication(sys.argv)
     try:
@@ -22,12 +33,17 @@ def main():
     window_layout = QtWidgets.QVBoxLayout()
     window.setLayout(window_layout)
 
-    test_label = QtWidgets.QLabel("Testing")
-    test_label.setAlignment(Qt.AlignLeft)
-    window_layout.addWidget(test_label)
+    radar_label = QtWidgets.QLabel("Radar Data")
+    radar_label.setAlignment(Qt.AlignCenter)
+    window_layout.addWidget(radar_label)
+
+    radar_image = RadarImage()
+    radar_image.setAlignment(Qt.AlignCenter)
+    window_layout.addWidget(radar_image)
 
     window.show()
     sys.exit(app.exec_())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
