416ed264bdead27b009d3006b7d6ea53b1730faf
Cameron Reikes <cameronreikes@gmail.com>
2019-05-25 19:21:58 -0700

    Add log constant and log function to data

--- src/radar_display.py
+++ src/radar_display.py
@@ -28,6 +28,7 @@ TCP_PORT = 54321
 UDP_PING_MESSAGE = b"ready"
 SOCKET_TIMEOUT = 3.0
 DEFAULT_VMAX = 255.0
+DEFAULT_LOG_CONSTANT = 20.0
 
 # f = BytesIO()
 # np.savez_compressed(
@@ -74,6 +75,7 @@ class DataThread(QThread):
         ).__init__()  # not sure why syntax has to be this way, errors if not
         self.sock = sock
         self.vmax = DEFAULT_VMAX
+        self.log_constant = DEFAULT_LOG_CONSTANT
         self.stopping = False
         # self.data = bytearray(IMAGE_DIMENSIONS[0] * IMAGE_DIMENSIONS[1])
         self.radar_image = radar_image
@@ -104,6 +106,8 @@ class DataThread(QThread):
             m = cm.ScalarMappable(norm=norm, cmap=cmap)
             final_data = np.frombuffer(data, dtype="f")
             final_data.shape = (IMAGE_DIMENSIONS[0], IMAGE_DIMENSIONS[1])
+            log_function = lambda x: np.log(x) * self.log_constant
+            final_data = log_function(final_data)
             # logging.debug(f"{m.to_rgba(127.0)}")
             ultra_final_data = m.to_rgba(final_data, bytes=True)
             # logging.debug(f"{ultra_final_data[0]}")
@@ -122,6 +126,13 @@ class DataThread(QThread):
             self.vmax = DEFAULT_VMAX
         else:
             self.vmax = float(new_vmax)
+
+    @Slot(str)
+    def new_log_constant(self, new_log_constant):
+        if new_log_constant == "":
+            self.log_constant = DEFAULT_LOG_CONSTANT
+        else:
+            self.log_constant = float(new_log_constant)
 
 
 def main():
@@ -162,12 +173,19 @@ def main():
         data_thread.new_data.connect(radar_image.update)
         data_thread.start()
 
-        vmax_linedit = QtWidgets.QLineEdit()
-        vmax_linedit.text = str(DEFAULT_VMAX)
-        vmax_linedit.setValidator(QtGui.QDoubleValidator())
-        vmax_linedit.textChanged.connect(data_thread.new_vmax)
+        vmax_lineedit = QtWidgets.QLineEdit()
+        vmax_lineedit.text = str(DEFAULT_VMAX)
+        vmax_lineedit.setValidator(QtGui.QDoubleValidator())
+        vmax_lineedit.textChanged.connect(data_thread.new_vmax)
         properties_layout.addWidget(QtWidgets.QLabel("Colorramp VMax: "), 0, 0, 1, 1)
-        properties_layout.addWidget(vmax_linedit, 0, 1, 1, 1)
+        properties_layout.addWidget(vmax_lineedit, 0, 1, 1, 1)
+
+        log_constant_lineedit = QtWidgets.QLineEdit()
+        log_constant_lineedit.text = str(DEFAULT_LOG_CONSTANT)
+        log_constant_lineedit.setValidator(QtGui.QDoubleValidator())
+        log_constant_lineedit.textChanged.connect(data_thread.new_log_constant)
+        properties_layout.addWidget(QtWidgets.QLabel("Log Constant: "), 1, 0, 1, 1)
+        properties_layout.addWidget(log_constant_lineedit, 1, 1, 1, 1)
 
         window.show()
         qt_exit_code = app.exec_()
